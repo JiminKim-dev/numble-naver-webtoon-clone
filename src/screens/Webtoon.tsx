d2621df74834038d1989a2380c294772e08ced95
도비 <jm.dev.study@gmail.com>
2023-03-09 17:44:30 +0900

    💄 임시적인 MainBanner 컴포넌트 생성 - 헤더와 탭뷰 사이의 공간을 만들기 위해 임시적으로 만들었다. 추후 수정예정

--- src/screens/Webtoon.tsx
+++ src/screens/Webtoon.tsx
@@ -1,11 +1,13 @@
 import { View, StyleSheet } from 'react-native';
 
 import Header from '@/components/Header';
+import MainBanner from '@/components/Banner/MainBanner';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Header />
+      <MainBanner />
     </View>
   );
 }
