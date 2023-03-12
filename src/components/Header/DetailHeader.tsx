dd2bfc5232143f3df000e611d52a43730a5a91c1
도비 <jm.dev.study@gmail.com>
2023-03-12 16:49:21 +0900

    ✨ 구독 버튼을 눌렀을때 알림 설정을 활성화 하는 기능 구현 - 전역 상태를 가지고 있지 않아 페이지 이동시 초기화 된다.

--- src/components/Header/DetailHeader.tsx
+++ src/components/Header/DetailHeader.tsx
@@ -1,18 +1,25 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { Ionicons, EvilIcons, MaterialCommunityIcons } from '@expo/vector-icons';
-
 import { DetailScreenProps, MyScreenProps } from '@/types/navigation';
+
+import { Ionicons, MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
 import { HEIGHTS, scale } from '@/styles/dimensions';
+
+interface SubscribeState {
+  id: number;
+  subscribe: boolean;
+  notification: boolean;
+}
 
 export default function DetailHeader() {
   const detailNavigation = useNavigation<DetailScreenProps['navigation']>();
   const myScreenNavigation = useNavigation<MyScreenProps['navigation']>();
   const {
-    params: { from, title },
+    params: { from, title, id: paramsId },
   } = useRoute<DetailScreenProps['route']>();
 
-  const onPressHandler = () => {
+  const backBtnPressHandler = () => {
     const moveMyScreen = () => {
       detailNavigation.pop();
       myScreenNavigation.navigate('MyScreen');
@@ -21,21 +28,81 @@ export default function DetailHeader() {
     return from === 'MyScreen' ? moveMyScreen() : detailNavigation.pop();
   };
 
+  const [subscribeState, setSubscribeState] = useState<SubscribeState | null>(null);
+  const [checkSubscribeButton, setCheckSubscribeButton] = useState<boolean>(false);
+  const [notificationActive, setNotificationActive] = useState<boolean>(false);
+
+  const showNotificationSettings = (id: number) =>
+    Alert.alert('', '관심 웹툰 알림설정이 꺼져있어요.\n알림을 켜고 업데이트 소식을 받으시겠어요?', [
+      {
+        text: 'Cancel',
+        onPress: () => setSubscribeState({ id, subscribe: true, notification: false }),
+        style: 'cancel',
+      },
+      { text: 'OK', onPress: () => setSubscribeState({ id, subscribe: true, notification: true }) },
+    ]);
+
+  const SubscribeBtnPressHandler = (id: number) => {
+    if (!subscribeState) {
+      setNotificationActive(true);
+      setCheckSubscribeButton(true);
+      return showNotificationSettings(id);
+    }
+
+    if (subscribeState.subscribe) {
+      setNotificationActive(false);
+      setCheckSubscribeButton(false);
+      return setSubscribeState({ ...subscribeState, subscribe: !subscribeState.subscribe, notification: false });
+    }
+
+    setNotificationActive(true);
+    return setSubscribeState({ ...subscribeState, subscribe: !subscribeState.subscribe, notification: true });
+  };
+
+  const notificationBtnPressHandler = () => {
+    if (!subscribeState) return;
+    // eslint-disable-next-line consistent-return
+    return setSubscribeState({ ...subscribeState, notification: !subscribeState.notification });
+  };
+
+  const isSubscribe = subscribeState?.subscribe;
+
+  const subscribeButtonName = () => {
+    if (isSubscribe) {
+      return 'checkcircle';
+    }
+    if (checkSubscribeButton) {
+      return 'pluscircle';
+    }
+    return 'pluscircleo';
+  };
+
   return (
     <View style={styles.header}>
       <View style={{ ...styles.headerContent, ...styles.contentGap }}>
-        <Pressable onPress={onPressHandler}>
+        <Pressable onPress={backBtnPressHandler}>
           <Ionicons name="chevron-back" size={24} color="black" />
         </Pressable>
         <Text style={styles.contentTitle}>{title}</Text>
       </View>
       <View style={styles.headerContent}>
-        <Pressable style={{ ...styles.headerContent, ...styles.subscribe }}>
-          <EvilIcons name="plus" size={24} color="black" />
+        <Pressable
+          style={{ ...styles.headerContent, ...styles.subscribe, ...styles.contentGap }}
+          onPress={() => SubscribeBtnPressHandler(paramsId)}
+        >
+          <AntDesign
+            name={subscribeButtonName() as 'pluscircle' | 'checkcircle' | 'pluscircleo'}
+            size={20}
+            color={isSubscribe || checkSubscribeButton ? 'green' : 'black'}
+          />
           <Text>관심</Text>
         </Pressable>
-        <Pressable>
-          <MaterialCommunityIcons name="bell-off-outline" size={24} color="black" />
+        <Pressable onPress={notificationBtnPressHandler} style={!notificationActive && styles.hide}>
+          <MaterialCommunityIcons
+            name={subscribeState?.notification ? 'bell' : 'bell-off-outline'}
+            size={24}
+            color={subscribeState?.notification ? 'orange' : 'black'}
+          />
         </Pressable>
       </View>
     </View>
@@ -60,12 +127,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   contentGap: {
-    gap: scale(8),
+    gap: scale(4),
   },
   contentTitle: {
     fontSize: scale(16),
   },
   subscribe: {
     paddingRight: scale(12),
+  },
+  hide: {
+    display: 'none',
   },
 });
