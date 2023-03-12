2c1f15425439ff5800f98b08740aa7508fcbb7a7
도비 <jm.dev.study@gmail.com>
2023-03-12 14:51:18 +0900

    ✨ mock 데이터를 사용하여 DetailScreen에 FlatList 배치

--- src/screens/WebtoonDetail.tsx
+++ src/screens/WebtoonDetail.tsx
@@ -1,18 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { DetailScreenProps } from '@/types/navigation';
+import { FlatList, Pressable, StyleSheet, View } from 'react-native';
+
+import { Feather } from '@expo/vector-icons';
 import DetailHeader from '@/components/Header/DetailHeader';
+import Card from '@/components/Card';
+import { scale } from '@/styles/dimensions';
 
-export default function DetailScreen({ route }: DetailScreenProps) {
-  const { id, title } = route.params;
+import { makeMockWebtoonList } from '@/utils/mockWebtoonList';
 
+export default function DetailScreen() {
   return (
     <View style={styles.container}>
       <DetailHeader />
-      <View style={styles.item}>
-        <Text>
-          웹툰 상세 id: {id}, title: {title}
-        </Text>
-      </View>
+
+      <FlatList
+        contentContainerStyle={styles.FlatListContainer}
+        data={makeMockWebtoonList(20).reverse()}
+        renderItem={({ item, index }) => (
+          <View style={styles.itemContainer}>
+            <Card
+              cardData={item}
+              cardStyle={{ imageSize: 'tiny', direction: 'horizontal' }}
+              episode={makeMockWebtoonList(20).length - index}
+            />
+          </View>
+        )}
+      />
+      <Pressable style={styles.arrow} onPress={() => {}}>
+        <Feather name="arrow-up" size={32} color="black" />
+      </Pressable>
     </View>
   );
 }
@@ -22,10 +37,17 @@ const styles = StyleSheet.create({
     flex: 1,
     position: 'relative',
   },
-  item: {
+  FlatListContainer: { backgroundColor: '#fff' },
+  itemContainer: {
     flex: 1,
+    flexDirection: 'row',
+    margin: scale(8),
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'skyblue',
+  },
+  arrow: {
+    position: 'absolute',
+    bottom: scale(40),
+    right: scale(20),
   },
 });
