0aba2e5624bbe20585f3a0c941de7f685c150a34
도비 <jm.dev.study@gmail.com>
2023-03-12 14:47:27 +0900

    ✨ mock 데이터를 사용하여 MyScreen에 FlatList 배치

--- src/screens/My.tsx
+++ src/screens/My.tsx
@@ -1,25 +1,44 @@
-import { Button, StyleSheet, View } from 'react-native';
+import { FlatList, Pressable, StyleSheet, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { DetailScreenProps } from '@/types/navigation';
+
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Card from '@/components/Card';
+import { scale } from '@/styles/dimensions';
+import { makeMockWebtoonList } from '@/utils/mockWebtoonList';
 
 export default function MyScreen() {
   const navigation = useNavigation<DetailScreenProps['navigation']>();
 
   return (
-    <View style={styles.container}>
-      <Button
-        title="웹툰 상세 페이지로 가기"
-        onPress={() => navigation.navigate('DetailScreen', { id: 1, title: '웹툰1', from: 'MyScreen' })}
-      />
-    </View>
+    <FlatList
+      contentContainerStyle={styles.FlatListContainer}
+      data={makeMockWebtoonList(13)}
+      renderItem={({ item }) => (
+        <View style={styles.itemContainer}>
+          <Pressable
+            onPress={() =>
+              navigation.navigate('DetailScreen', { id: Number(item.mastrId), title: item.title, from: 'MyScreen' })
+            }
+          >
+            <Card cardData={item} cardStyle={{ imageSize: 'tiny', direction: 'horizontal' }} />
+          </Pressable>
+          <Pressable style={{}} onPress={() => {}}>
+            <MaterialCommunityIcons name="bell" size={24} color="green" />
+          </Pressable>
+        </View>
+      )}
+    />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+export const styles = StyleSheet.create({
+  FlatListContainer: { backgroundColor: '#fff' },
+  itemContainer: {
     flex: 1,
-    backgroundColor: '#ffcdd2',
+    flexDirection: 'row',
+    margin: scale(8),
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
